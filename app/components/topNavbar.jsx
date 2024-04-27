7d6609501ced6c42ba0926f81f8c245fc3037b2c
RInf0 <c14h3123@gmail.com>
2024-04-28 01:34:45 +0700

    login

--- app/components/topNavbar.jsx
+++ app/components/topNavbar.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
-import Image from 'next/image'
-import Logo from '../assets/images/logo.png'
-import Pita from '../assets/images/pita.png'
-import Bg from '../assets/images/bg.png'
+// components/TopNavbar.js
+import React from 'react';
+import Image from 'next/image';
+import Logo from '../assets/images/logo.png';
+import Pita from '../assets/images/pita.png';
+import Bg from '../assets/images/bg.png';
 
 export default function TopNavbar() {
   return (
-    <div style={{ position: 'relative', width: '100%', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+    <div style={{ position: 'absolute', width: '100%', height: '100vh', zIndex: '-1' }}>
+      <Image
+        src={Bg}
+        alt="Background"
+        layout="fill"
+        objectFit="cover"
+        placeholder="blur"
+        quality={100}
+        style={{ filter: 'brightness(100%)' }}
+      />
       <div className='text-[#AD773D] font-bold text-5xl absolute z-10 right-28 top-16'>Atma Kitchen</div>
       <div className='flex justify-between w-full items-center bg-[#171832] h-24 absolute top-10 z-10'></div>
       <Image
@@ -23,28 +33,6 @@ export default function TopNavbar() {
         height={100}
         className='absolute z-10 right-8 top-16'
       />
-      <div style={{ position: 'absolute', width: '100%', height: '100%', zIndex: '-1' }}>
-        <Image
-          src={Bg}
-          alt="Background"
-          layout="fill"
-          objectFit="cover"
-          placeholder="blur"
-          quality={100}
-          style={{ filter: 'brightness(100%)' }}
-        />
-      </div>
-      <div className="bg-white rounded-lg shadow-lg p-8 max-w-sm w-full size-48 md:size-96">
-        
-        <h1 className="text-[#171832] font-bold text-center mb-4">Welcome to Atma Kitchen</h1>
-        <p className="text-[#171832] text-center mb-8">Silahkan lakukan register bila belum punya akun</p>
-        <button className="bg-[#171832] text-white font-bold py-2 px-4 rounded-full w-full mb-4">
-          Login
-        </button>
-        <button className="bg-[#171832] text-white font-bold py-2 px-4 rounded-full w-full">
-          Register
-        </button>
-      </div>
-    </div>  
+    </div>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,11 +1,15 @@
+import React from 'react';
 import Login from "./page/login";
 import TopNavbar from "./components/topNavbar";
+import Card from "./components/card";
 
 export default function Home() {
   return (
-  <div className="container">
-    <TopNavbar></TopNavbar>
-    <Login></Login>
-  </div>
+    <>
+      <TopNavbar />
+      <div className="flex justify-center items-center h-screen">
+        <Card />
+      </div>
+    </>
   );
 }
