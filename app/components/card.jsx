57a52cead08a173fac3733549e7774279d0a51d9
stefanuspet <stefanuspetra.p@gmail.com>
2024-04-29 20:12:03 +0700

    update ui

--- app/components/card.jsx
+++ app/components/card.jsx
@@ -1,16 +1,19 @@
-// components/Card.js
-import React from 'react';
+import React from "react";
 
 export default function Card() {
   return (
     <div className="bg-white rounded-3xl p-8 md:w-[40rem] mt-10">
-      <h1 className="text-[#171832] font-bold text-2xl font-serif text-center mb-2">Welcome to Atma Kitchen</h1>
-      <p className="text-[#171832] font-serif text-center mb-10">Silahkan lakukan register bila belum punya akun</p>
+      <h1 className="text-[#171832] font-bold text-2xl font-serif text-center mb-2">
+        Welcome to Atma Kitchen
+      </h1>
+      <p className="text-[#171832] font-serif text-center mb-10">
+        Silahkan lakukan register bila belum punya akun
+      </p>
       <div>
-        <button className="bg-[#171832] text-white font-bold py-2 px-4 rounded-full w-full mb-4">
+        <button className="bg-[#171832] text-white font-bold py-2 px-4 rounded-full w-full mb-4 opacity-100">
           Login
         </button>
-        <button className="bg-[#171832] text-white font-bold py-2 px-4 rounded-full w-full mb-8">
+        <button className="bg-[#171832] text-white font-bold py-2 px-4 rounded-full w-full mb-8 opacity-100">
           Register
         </button>
       </div>
--- app/components/topNavbar.jsx
+++ app/components/topNavbar.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import Image from 'next/image';
-import Logo from '../assets/images/logo.png';
-import Pita from '../assets/images/pita.png';
-import Bg from '../assets/images/bg.png';
+import React from "react";
+import Image from "next/image";
+import Logo from "../assets/images/logo.png";
+import Pita from "../assets/images/pita.png";
+import Bg from "../assets/images/bg.png";
+import { IoMdArrowRoundBack } from "react-icons/io";
 
 export default function TopNavbar() {
   return (
-    <div style={{ position: 'absolute', width: '100%', height: '100vh', zIndex: '-1' }}>
+    <div
+      style={{
+        position: "absolute",
+        width: "100%",
+        height: "100vh",
+        zIndex: "-1",
+      }}
+    >
       <Image
         src={Bg}
         alt="Background"
@@ -14,25 +22,30 @@ export default function TopNavbar() {
         objectFit="cover"
         placeholder="blur"
         quality={100}
-        style={{ filter: 'brightness(100%)', opacity: '0.8' }} // Set opacity pada latar belakang
+        style={{ filter: "brightness(100%)" }}
       />
-      <div className='text-[#AD773D] font-bold text-5xl absolute z-10 right-28 top-16'>Atma Kitchen</div>
-      <div className='flex justify-between w-full items-center bg-[#171832] h-24 absolute top-10 z-10'></div>
+      {/* <div className='text-[#AD773D] font-bold text-5xl absolute z-10 right-28 top-16'>Atma Kitchen</div> */}
+      <div className="flex justify-between w-full items-center bg-[#171832] h-20 absolute top-8 z-10 opacity-80"></div>
+      <IoMdArrowRoundBack
+        size={40}
+        className="absolute z-10 left-6 top-14 cursor-pointer text-[#AD773D] "
+      />
       <Image
         src={Logo}
         alt="Logo"
         width={150}
         height={200}
-        className='absolute z-10 left-20 top-0'
-        style={{ opacity: '0.8' }} // Set opacity pada logo
+        className="absolute z-10 left-20 top-0"
       />
-      <div className='text-[#AD773D] font-bold font-serif text-3xl absolute z-10 right-28 top-14'>Atma Kitchen</div>
+      <div className="text-[#AD773D] font-bold font-serif text-3xl absolute z-10 right-28 top-14">
+        Atma Kitchen
+      </div>
       <Image
         src={Pita}
         alt="Pita"
         width={50}
         height={100}
-        className='absolute z-10 right-8 top-16'
+        className="absolute z-10 right-8 top-14"
       />
     </div>
   );
