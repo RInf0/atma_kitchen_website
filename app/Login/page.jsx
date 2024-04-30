6025959506bffc0e26cf327bc0d452baa42f3e01
stefanuspet <stefanuspetra.p@gmail.com>
2024-04-30 22:44:44 +0700

    login ui

--- app/Login/page.jsx
+++ app/Login/page.jsx
@@ -1,35 +1,74 @@
-import React from 'react';
-import TopNavbar from "../components/topNavbar";
-import { FaEye } from "react-icons/fa";
+"use client";
+import React, { useState } from "react";
+import TopNavbar from "../components/topNavbarWelcome";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
-export default function page() {
+export default function Page() {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
     <>
       <TopNavbar />
-      <div className="flex justify-center items-center h-screen">
-        <div className="bg-white rounded-3xl p-8 md:w-[40rem] mt-20 opacity-80 ">
-          <div className="bg-[#AD773D]"></div>
-          <h1 className="text-[#AD773D] font-bold font-serif text-center mb-0.5 text-3xl ">LOGIN</h1>
-          <hr style={{ borderWidth: '1.5px' }} />
-          <p className="text-[#171832] font-serif text-center mt-1 mb-8" style={{ fontSize: '14px' }}>WELCOME BACK!</p>
-          <div className="flex flex-col space-y-4 justify-end" style={{ marginLeft: '18%' }}>
-            <button className="bg-[#AD773D] text-white  py-2 px-4 rounded-full w-full md:w-[23rem] flex justify-between items-center">
-              Enter your email
-            </button>
-            <button className="bg-[#AD773D] text-white  py-2 px-4 rounded-full w-full md:w-[23rem] flex justify-between items-center">
-              <div>Enter your password</div>
-              <FaEye />
-            </button>
-          </div>
-          <div className="mt-6" style={{ marginLeft: '13%' }}>
-            <button className="bg-[#171832] text-white  py-2 px-4 rounded-full w-full md:w-[27rem] ">
-              Enter your email
-            </button>
-            <div className="flex" style={{ marginLeft: '40%' }}>
-              <p className="text-[#171832] font-serif text-center mt-1 mb-8" style={{ fontSize: '14px', marginLeft: '-25%' }}>Not have an account ?</p>
-              <p className="text-[#AD773D] font-serif text-center mt-1 mb-8" style={{ fontSize: '14px' }}>Register</p>
+      <div className="flex justify-center items-center h-screen w-full">
+        <div className="bg-white rounded-3xl p-8 md:w-[40rem] mt-20 bg-opacity-80 w-full">
+          <h1 className="text-[#AD773D] font-bold font-serif text-center mb-0.5 text-3xl ">
+            LOGIN
+          </h1>
+          <hr style={{ borderWidth: "1.5px" }} />
+          <p
+            className="text-[#171832] font-serif text-center mt-1 mb-8"
+            style={{ fontSize: "14px" }}
+          >
+            WELCOME BACK!
+          </p>
+          <form action="#" method="POST">
+            <div className="flex justify-center mt-5">
+              <input
+                type="email"
+                placeholder="Enter your email"
+                className="bg-[#AD773D] placeholder-white py-2 px-4 rounded-full w-full md:w-[23rem] flex justify-between items-center"
+              />
             </div>
-          </div>
+            <div className="flex justify-center mt-5 items-center relative">
+              <input
+                type={showPassword ? "text" : "password"}
+                placeholder="Enter your password"
+                className="bg-[#AD773D] placeholder-white py-2 px-4 rounded-full w-full md:w-[23rem] pr-12" // Tambahkan pr-12 untuk memberikan ruang di sebelah kanan input untuk ikon mata
+              />
+              <div className="absolute right-32 top-1/2 transform -translate-y-1/2">
+                {showPassword ? (
+                  <FaEyeSlash onClick={togglePasswordVisibility} />
+                ) : (
+                  <FaEye onClick={togglePasswordVisibility} />
+                )}
+              </div>
+            </div>
+
+            <div className="flex justify-center mt-5">
+              <button className="bg-[#011145] hover:bg-[#01071b] text-[#AD773D] py-2 px-4 rounded-full w-96 h-14 ">
+                Login
+              </button>
+            </div>
+            <div className="flex justify-center mt-5">
+              <p
+                className="text-[#171832] font-serif text-center mt-1 mb-8"
+                style={{ fontSize: "14px" }}
+              >
+                Not have an account ?{" "}
+                <a
+                  href="#"
+                  class="text-[#AD773D] font-serif text-center mt-1 mb-8 hover:teks-[#AD773D]"
+                  style={{ fontSize: "14px" }}
+                >
+                  Register
+                </a>
+              </p>
+            </div>
+          </form>
         </div>
       </div>
     </>
